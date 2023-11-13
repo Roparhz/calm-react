1b29f53447429b30ba4fd123312959814858dd5e
Roparhz <julien.leveque21@gmail.com>
2023-11-13 11:46:20 +0100

    dernière modif titre

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,7 +5,7 @@ import  "./Menu.css"
 function Menu(){
     return(
         <section className="section-menu">
-            <img src="/menu-06-11-23.png" alt="Menu de la semaine du 9 ocobre 2023" className="menu"/>
+            <img src="/menu-13-11-23.png" alt="Menu de la semaine du 13 novembre 2023" className="menu"/>
         </section>
     )
 
