00a6506943271f917f8807abce0df093432383fa
Roparhz <julien.leveque21@gmail.com>
2023-11-16 09:15:43 +0100

    ajout d'un loader au chargement

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideNav from "./components/SideNav/SideNav"
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Menu from "./components/Menu/Menu";
 import ImgSeason from "./components/Img-season/ImgSeason";
 import Footer from "./components/Footer/Footer";
+import Loader from "./components/Loader/Loader";
 import "./App.css";
 import "./css/helpers.css";
 import "./css/reset.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />; 
+  }
+
   return (
     <div className="App">
       <SideNav />
