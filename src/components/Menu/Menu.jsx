8a6fb88f89225d7710557d5c85422f93aa087193
Roparhz <julien.leveque21@gmail.com>
2023-11-24 19:24:53 +0100

    menu du 27/11

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,10 +6,10 @@ function Menu(){
     return (
       <section className="section-menu">
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-20-11-23.webp" />
-          <source srcset="img/menu-20-11-23-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-27-11-23.webp" />
+          <source srcset="img/menu-27-11-23-mini.webp" />
           <img
-            src="img/menu-20-11-23-mini.webp"
+            src="img/menu-27-11-23-mini.webp"
             alt="Menu de la semaine du 20 novembre 2023"
             className="menu"
             width="800"
