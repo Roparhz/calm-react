26b04a2e0ecd817078dc40370217fe19c795afde
Roparhz <julien.leveque21@gmail.com>
2023-12-02 15:35:32 +0100

    ajout menu de la semaine

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import  "./Menu.css"
-
-
 function Menu(){
     return (
       <section className="section-menu">
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-27-11-23.webp" />
-          <source srcset="img/menu-27-11-23-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-04-12-23.webp" />
+          <source srcset="img/menu-04-12-23-mini.webp" />
           <img
-            src="img/menu-27-11-23-mini.webp"
-            alt="Menu de la semaine du 20 novembre 2023"
+            src="img/menu-04-12-23-mini.webp"
+            alt="Menu de la semaine du 4 décembre 2023"
             className="menu"
             width="800"
             height="600"
