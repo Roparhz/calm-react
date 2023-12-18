63de9841fb83ea65ac03357fe7dc0c2b59e5882d
Roparhz <julien.leveque21@gmail.com>
2023-12-18 08:11:00 +0100

    changement du menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,12 +3,17 @@ import  "./Menu.css"
 function Menu(){
     return (
       <section className="section-menu">
+        <h2 className="h2-menu">"Semaine de l'avent"</h2>
+        {/* <p>
+          les plats que vous avez le plus apprécié durant cette saison regroupé
+          en un seul menu !
+        </p> */}
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-04-12-23.webp" />
-          <source srcset="img/menu-04-12-23-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-18-12-23.webp" />
+          <source srcset="img/menu-18-12-23-mini.webp" />
           <img
-            src="img/menu-04-12-23-mini.webp"
-            alt="Menu de la semaine du 4 décembre 2023"
+            src="img/menu-18-12-23-mini.webp"
+            alt="Menu de la semaine du 18 décembre 2023"
             className="menu"
             width="800"
             height="600"
