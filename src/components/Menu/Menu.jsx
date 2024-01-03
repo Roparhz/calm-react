06d758f4d0fe811f006a9d5f4ad382df7ff8d213
Roparhz <julien.leveque21@gmail.com>
2024-01-03 09:29:04 +0100

    menu janvier

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,24 +3,24 @@ import  "./Menu.css"
 function Menu(){
     return (
       <section className="section-menu">
-        <h2 className="h2-menu">Fermeture hivernale</h2>
-        <p>
-          Retour le mercredi 3 pour une nouvelle année !
-        </p>
-        {/* <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-18-12-23.webp" />
-          <source srcset="img/menu-18-12-23-mini.webp" />
+        <h2 className="h2-menu">
+          CALM vous souhaite une très belle année 2024 !
+        </h2>
+        <p className="p-menu">Menu du 3 au 12 janvier</p>
+        <picture>
+          <source media="(min-width: 1025px)" srcset="img/menu-03-01-24.webp" />
+          <source srcset="img/menu-03-01-24-mini.webp" />
           <img
-            src="img/menu-18-12-23-mini.webp"
-            alt="Menu de la semaine du 18 décembre 2023"
+            src="img/menu-03-01-24-mini.webp"
+            alt="Menu de la semaine du 03 janvier 2024"
             className="menu"
             width="800"
             height="600"
           />
-        </picture> */}
+        </picture>
       </section>
     );
 
 }
 
-export default Menu
+export default Menu;
