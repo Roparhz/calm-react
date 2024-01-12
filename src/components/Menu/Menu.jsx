6fe8fc3bbac0c1997ea80319cf00707ce167a82e
Roparhz <julien.leveque21@gmail.com>
2024-01-12 09:43:52 +0100

    changement menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,13 +6,13 @@ function Menu(){
         <h2 className="h2-menu">
           CALM vous souhaite une très belle année 2024 !
         </h2>
-        <p className="p-menu">Menu du 3 au 12 janvier</p>
+        <p className="p-menu">Menu du 15 au 19 janvier</p>
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-03-01-24.webp" />
-          <source srcset="img/menu-03-01-24-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-15-01-24.webp" />
+          <source srcset="img/menu-15-01-24-mini.webp" />
           <img
-            src="img/menu-03-01-24-mini.webp"
-            alt="Menu de la semaine du 03 janvier 2024"
+            src="img/menu-15-01-24-mini.webp"
+            alt="Menu de la semaine du 15 janvier 2024"
             className="menu"
             width="800"
             height="600"
