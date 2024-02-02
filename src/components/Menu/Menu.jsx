b0b7d98aa3508ec3f33ad6c4076a32ba3ec629d0
Roparhz <julien.leveque21@gmail.com>
2024-02-02 17:12:51 +0100

    ajout menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,13 +6,13 @@ function Menu(){
         <h2 className="h2-menu">
           CALM vous souhaite une très belle année 2024 !
         </h2>
-        <p className="p-menu">Menu du 22 janvier</p>
+        <p className="p-menu">Menu du 5 février</p>
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-22-01-24.webp" />
-          <source srcset="img/menu-22-01-24-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-05-02-24.webp" />
+          <source srcset="img/menu-05-02-24-mini.webp" />
           <img
-            src="img/menu-22-01-24-mini.webp"
-            alt="Menu de la semaine du 22 janvier 2024"
+            src="img/menu-05-02-24-mini.webp"
+            alt="Menu de la semaine du 5 février 2024"
             className="menu"
             width="800"
             height="600"
