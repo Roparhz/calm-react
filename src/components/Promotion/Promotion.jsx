4788029e056fc09b38426b0356bbe6af44c54efb
Roparhz <julien.leveque21@gmail.com>
2024-02-02 13:33:25 +0100

    ajout menu st valentin

--- src/components/Promotion/Promotion.jsx
+++ src/components/Promotion/Promotion.jsx
@@ -4,14 +4,14 @@ function Promotion() {
   return (
     <section className="section-promotion">
       <h2 className="h2-promotion">
-        CALM vous propose un plat de saison sur commande uniquement !
+        CALM vous propose un plat de la St Valentin sur commande uniquement !
       </h2>
       <picture>
-        <source media="(min-width: 1025px)" srcset="img-promotion/promotion-19-01-24.webp" />
-        <source srcset="img-promotion/promotion-19-01-24-mini.webp" />
+        <source media="(min-width: 1025px)" srcset="img-promotion/menu-st-valentin.webp" />
+        <source srcset="img-promotion/menu-st-valentin-mini.webp" />
         <img
-          src="img-promotion/promotion-19-01-24.webp"
-          alt="Promotion de la semaine du 19 janvier 2024"
+          src="img-promotion/menu-st-valentin.webp"
+          alt="Menu de la St Valentin"
           className="promotion"
           width="800"
           height="600"
