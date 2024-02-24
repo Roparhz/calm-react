1fb87aa1cb09676f381c2cefdd88b132b5428833
Roparhz <julien.leveque21@gmail.com>
2024-02-24 15:31:47 +0100

    changement menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,13 +3,13 @@ import  "./Menu.css"
 function Menu(){
     return (
       <section className="section-menu">
-        <p className="p-menu">Menu du 19 février</p>
+        <p className="p-menu">Menu du 26 février</p>
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-19-02-24.webp" />
-          <source srcset="img/menu-19-02-24-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-26-02-24.webp" />
+          <source srcset="img/menu-26-02-24-mini.webp" />
           <img
-            src="img/menu-19-02-24-mini.webp"
-            alt="Menu de la semaine du 19 février 2024"
+            src="img/menu-26-02-24-mini.webp"
+            alt="Menu de la semaine du 26 février 2024"
             className="menu"
             width="800"
             height="600"
