fc8905861cbbd51b09c7f7ff8c962ee3be9dd0f1
Roparhz <julien.leveque21@gmail.com>
2024-03-15 15:03:25 +0100

    chgmt menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,13 +3,13 @@ import  "./Menu.css"
 function Menu(){
     return (
       <section className="section-menu">
-        <p className="p-menu">Menu de la semaine du 4 mars</p>
+        <p className="p-menu">Menu de la semaine du 18 mars</p>
         <picture>
-          <source media="(min-width: 1025px)" srcset="img/menu-04-03-24.webp" />
-          <source srcset="img/menu-04-03-24-mini.webp" />
+          <source media="(min-width: 1025px)" srcset="img/menu-18-03-24.webp" />
+          <source srcset="img/menu-18-03-24-mini.webp" />
           <img
-            src="img/menu-04-03-24-mini.webp"
-            alt="Menu de la semaine du 4 mars 2024"
+            src="img/menu-18-03-24-mini.webp"
+            alt="Menu de la semaine du 18 mars 2024"
             className="menu"
             width="800"
             height="600"
